972001656393404b417636833226ad2132f8bdd3
saurabh.upadhyay <saurabh.upadhyay@mountblue.tech>
2021-04-08 11:44:59 +0530

    devise route error fix

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,10 @@ Rails.application.routes.draw do
   devise_for :users
   root 'home#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  # config/routes.rb
+  devise_scope :user do
+    get '/users', to: 'devise/registrations#new'
+    get '/users/password', to: 'devise/passwords#new'
+  end
+  
 end
