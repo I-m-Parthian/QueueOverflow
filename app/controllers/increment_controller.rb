3d9d6429220fa3c2f40e217c6e9270703c0c681f
saurabh.upadhyay <saurabh.upadhyay@mountblue.tech>
2021-04-09 16:31:53 +0530

    ready to merge

--- app/controllers/increment_controller.rb
+++ app/controllers/increment_controller.rb
@@ -1,9 +1,11 @@
 class IncrementController < ApplicationController
     def increment_votes
-        p request.params
         a = request.params[:id]
+
         vote_type = request.params[:format]
+
         @answer = Answer.find(a)
+        
         if vote_type == 'up_vote'
             @answer.increment!(:up_vote)
         elsif vote_type == 'down_vote'
